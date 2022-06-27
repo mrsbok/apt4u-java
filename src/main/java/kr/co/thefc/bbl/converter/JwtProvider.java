3041f3a0c87dc8601285d73459fcc73bd6e96d93
mrsbok <swingebk@gmail.com>
2022-06-27 15:14:06 +0900

    토큰 생성 수정

--- src/main/java/kr/co/thefc/bbl/converter/JwtProvider.java
+++ src/main/java/kr/co/thefc/bbl/converter/JwtProvider.java
@@ -5,11 +5,14 @@ import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.swagger.models.auth.In;
+
+import javax.lang.model.type.NullType;
 
 public class JwtProvider {
 
 	public String tokenKey  = "bbl";
-	public String jwtCreater(Integer idx){
+	public String jwtCreater(Integer trainerIdx, Integer userIdx){
 		//토큰 생성시간
 		LocalDateTime createDate = LocalDateTime.now();
 
@@ -18,7 +21,8 @@ public class JwtProvider {
 
 		//토큰 내용
 		Map<String,Object> claims = Map.of(
-				"trainerIdx" , idx,
+				"trainerIdx" , trainerIdx,
+				"userIdx" , userIdx,
 				"createdDate" , createDate.format(DateTimeFormatter.ISO_DATE_TIME)
 		);
 
