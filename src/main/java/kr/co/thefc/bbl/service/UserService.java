f5e32996f1c45fb9031432478d8aa6a0b85d1710
one-wework <o970122@gmail.com>
2022-07-13 14:54:40 +0900

    20220713 - jhj * PT 이용권 검색 추가 * PT톡 검색 추가

--- src/main/java/kr/co/thefc/bbl/service/UserService.java
+++ src/main/java/kr/co/thefc/bbl/service/UserService.java
@@ -1,9 +1,14 @@
 package kr.co.thefc.bbl.service;
 
 import java.util.HashMap;
+import java.util.List;
 
 public interface UserService {
+    public List<HashMap> getPTLessonVoucherSellerInfo(List<HashMap> list, HashMap map);
+
     public HashMap getPTTrainerDetail(HashMap map);
 
     public HashMap getPTLessonVoucherDetail(HashMap map);
+
+    public String phoneFormat(String number);
 }
